a57ebf3d726b25e01eb2dae246bfca2caec042b8
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-09-10 19:53:46 +0100

    update asset functionality

--- splash/asset.py
+++ splash/asset.py
@@ -12,19 +12,22 @@ from core import sprites
 class Asset(Element):
   '''An image asset.'''
 
-  def __init__(self, id, pos, image: str, display = list()):
-    '''Create an image asset.'''
+  def __init__(self, id, pos, image: str, display = None):
+    '''Create an image asset.
 
-    super().__init__(sprites.splash)
+    ｜ parameter | type | description |
+    | :--------- | :-- | :----------- |
+    | `image` | The file name from which to load the image asset. |
+
+    Other base parameters are inherited from `splash.Element`.
+    '''
+
+    super().__init__(id = id, pos = pos, display = display, groups = sprites.splash)
 
     self.image = py.image.load(f"assets/{image}").convert()
     self.rect = self.image.get_rect()
-    self.rect.x, self.rect.y = pos
-
-    self.id = id
-    self.display = display
 
   def update(self):
-    self.visible()
+    super().visible()
 
     ## TODO
