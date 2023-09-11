1c02c575962258b1b8666f32808e0b3396cacc21
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-09-11 09:51:56 +0100

    fix

--- splash/text.py
+++ splash/text.py
@@ -5,53 +5,51 @@ Text rendering
 import pygame as py
 from pygame import freetype
 
-from splash.element import Element
 from core import sprites, ui
 import util
+
+from splash.element import Element
 
 
 freetype.init()
 
 
-class Style:
-  '''A text style.'''
-
-  def __init__(self, *, typeface = None, size = None, col = None, align = None):
-    '''Create a text style.
-
-    Arguments are set to internal defaults if unspecified.
-
-    `align` should be a tuple of 2 `int`s, representing x and y alignment, respectively. `0` for centre, `1` for right / bottom, `-1` for left / top.
-    '''
-
-    self.typeface = typeface or ui.font.typeface
-    self.size = size or ui.font.size
-    self.col = col or ui.col.text
-    self.align = align or (0, 0)
-
-
 class Text(Element):
   '''A static text element.'''
 
-  def __init__(self, id, pos, text, display = list(), style = Style()):
+  class Style:
+    '''A text style.'''
+  
+    def __init__(self, *, typeface = None, size = None, col = None, align = None):
+      '''Create a text style.
+  
+      Arguments are set to internal defaults if unspecified.
+  
+      `align` should be a tuple of 2 `int`s, representing x and y alignment, respectively. `0` for centre, `1` for right / bottom, `-1` for left / top.
+      '''
+  
+      self.typeface = typeface or ui.font.typeface
+      self.size = size or ui.font.size
+      self.col = col or ui.col.text
+      self.align = align or (0, 0)
+
+  def __init__(self, id, pos, text, style = None, display = None):
     '''Create a text element.
     
-    | argument | type | description |
-    | :------- | :--- | :---------- |
-    | `id` | | Unique attribute to identify text element. |
-    | `pos` | `[int, int]` | Coordinate where text element is positioned. |
+    | parameter | type | description |
+    | :-------- | :--- | :---------- |
     | `text` | `str` | Text to display. |
-    | `display` | `list[str]` | List of screen states where button will be shown. |
-    | `style` | `splash.text.Style` | Dictionary of style settings for text. |
+    | `style` | `Text.Style` | Style settings for text. |
+
+    Other base parameters are inherited from `splash.Element`.
     '''
 
-    super().__init__(id = id, pos = pos, display = display, groups = sprites.splash)
+    super().__init__(id, pos, display = display, groups = sprites.splash)
 
     self.text = text
-    self.style = style
+    self.style = style or Style()
     
-    if self.text:
-      self.image, self.rect = Text.render(text = self.text, style = self.style)
+    self.image, self.rect = Text.render(text = self.text, style = self.style)
 
   def update(self):
     self.visible()
