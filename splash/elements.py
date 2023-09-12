085d1d5908a23af737028192b164408ac67528db
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-09-12 15:41:34 +0100

    add base interaction functionality

--- splash/elements.py
+++ splash/elements.py
@@ -30,12 +30,19 @@ class Displayed:
 class Element(py.sprite.Sprite):
   '''Base class from which all splash sprites derive.'''
 
-  def __init__(self, id: str, pos = None, display = None, groups = None):
+  def __init__(self,
+    id: str,
+    pos = None,
+    interact = False,
+    display = None,
+    groups = None,
+  ):
     '''
     | parameter | type | description |
     | :-------- | :--- | :---------- |
     | `id` | `str` | Unique ID to identify element. |
     | `pos` | `[num, num]` | Coordinates to position element. |
+    | `interact` | `bool` | Whether element can be hovered or clicked. |
     | `display` | `splash.Displayed` | Sprite display settings. |
     | `groups` | `list[py.sprite.Group]` | Groups to add element to. |
     '''
@@ -49,6 +56,10 @@ class Element(py.sprite.Sprite):
     self.x = xy[0]
     self.y = xy[1]
 
+    if interact:
+      self.hover = False
+      self.click = False
+
   def visible(self):
     '''Show or hide sprite depending on current screen state.'''
 
@@ -56,6 +67,34 @@ class Element(py.sprite.Sprite):
       sprites.active.add(self, layer = self.display.layer)
     else:
       sprites.active.remove(self)
+
+  def interact(self, root = None) -> str:
+    '''Detect hover and click interactions with element and return state as a string.
+
+    If element is clicked, call `root` if passed in, or `self.root` if available.
+    '''
+
+    down = py.mouse.get_pressed()[0]
+
+    if self.rect.collidepoint(py.mouse.get_pos()):
+      if not down:  # hovered
+        if self.click:  # clicked and released
+          if root:
+            root()
+          elif hasattr(self, "root"):
+            self.root()
+        self.hover = True
+        self.click = False
+        return "hover"
+
+      elif down and self.hover:  # clicked
+        self.click = True
+        return "click"
+
+    else:  # not hovering
+      self.hover = False
+      self.click = False
+      return "idle"
 
   @ property
   def pos(self):
