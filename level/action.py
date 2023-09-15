0d56630c6bce5b2833ab2581555bde3a1dbed954
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-09-15 12:41:24 +0100

    update docstring

--- level/action.py
+++ level/action.py
@@ -6,23 +6,27 @@ Level events
 class Action:
   '''An event that can be triggered within a level.'''
 
-  def __init__(self, beat, action):
-    '''Create an event.
+  def __init__(self, beat, action, loop = None):
+    '''Create an action event.
     
-    | argument | type | description |
-    | :------- | :--- | :---------- |
-    | `beat` | `int`, `float` | The beat on which action should occur. |
-    | `action` | `Callable` | The function to call when action occurs. |
+    | parameter | type | description |
+    | :-------- | :--- | :---------- |
+    | `beat` | `num` | Beat on which event should occur. |
+    | `action` | `Callable` | Function called when event occurs. |
+    | `loop` | `int, num` | How many times to loop the action, and the gap in beats between each repeat. |
     '''
 
     self.beat = beat
     self.activate = action
+    self.loop = loop
+    self.looped = 0
 
 
 class Hint(Action):
   '''Text that displays during a level.'''
   
   def __init__(self, *args, **kwargs):
-    '''Create a text hint.'''
+    '''Create a level hint.
+    '''
 
     super().__init__(*args, **kwargs)
