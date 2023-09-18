7212f40a5b811c63476991482cbc5ae9d299839c
Sup#2.0 <sup2point0@gmail.com>
2023-09-18 18:02:53 +0100

    add blur functionality

--- effects/blur.py
+++ effects/blur.py
@@ -9,7 +9,11 @@ import PIL as pil
 def load(file: str, blur: int) -> py.Surface:
   '''Load and blur a file into a pygame surface.'''
 
-  ...
+  image = pil.Image.open(file)
+  image = image.filter(pil.ImageFilter.GaussianBlur(blur))
+  image = py.image.fromstring(image.tobytes(), image.size, image.mode)
+
+  return image
 
 
 def process(image: py.Surface, blur: int) -> py.Surface:
