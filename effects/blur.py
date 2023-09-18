c1cd65747c4268e09ca388b58a595176efd4e396
Sup#2.0 <sup2point0@gmail.com>
2023-09-18 17:34:45 +0100

    add new blur effect file

---
+++ effects/blur.py
@@ -0,0 +1,18 @@
+'''
+Blur effects
+'''
+
+import pygame as py
+import PIL as pil
+
+
+def load(file: str, blur: int) -> py.Surface:
+  '''Load and blur a file into a pygame surface.'''
+
+  ...
+
+
+def process(image: py.Surface, blur: int) -> py.Surface:
+  '''Blur an existing pygame surface.'''
+
+  ...
