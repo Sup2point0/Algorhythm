e610f48adfb020a08f4232f33c2c28ce48fc3aa3
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-09-20 09:27:48 +0100

    finish blur functions

--- effects/blur.py
+++ effects/blur.py
@@ -6,17 +6,23 @@ import pygame as py
 from PIL import Image, ImageFilter
 
 
+def _blur_(image, blur) -> py.Surface:
+  '''Internal utility function to blur a Pillow image.'''
+
+  out = image.filter(ImageFilter.GaussianBlur(blur))
+  out = py.image.fromstring(out.tobytes(), out.size, out.mode)
+  return out
+
+
 def load(file: str, blur: int) -> py.Surface:
   '''Load and blur a file into a pygame surface.'''
 
   image = Image.open(file)
-  image = image.filter(ImageFilter.GaussianBlur(blur))
-  image = py.image.fromstring(image.tobytes(), image.size, image.mode)
-
-  return image
+  return _blur_(image, blur)
 
 
-def process(image: py.Surface, blur: int) -> py.Surface:
+def process(surf: py.Surface, blur: int) -> py.Surface:
   '''Blur an existing pygame surface.'''
 
-  ...
+  image = Image.frombytes("RGBA", surf.get_size(), py.image.tostring(surf, "RGBA"))
+  return _blur_(image, blur)
