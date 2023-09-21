4f21ae8c23f4219104cbb38c9bb81a4d608b63c2
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-09-21 08:45:24 +0100

    add hint functionality

--- level/action.py
+++ level/action.py
@@ -24,9 +24,83 @@ class Action:
 
 class Hint(Action):
   '''Text that displays during a level.'''
-  
-  def __init__(self, *args, **kwargs):
+
+  class Highlight:
+    '''A highlighted (non-darkened) part of the hint overlay.'''
+
+    def __init__(self, pos, size):
+      '''Create a rectangular highlighted area.
+
+      | parameter | type | description |
+      | :-------- | :--- | :---------- |
+      | `pos` | `num, num` | Position of the rectangle (centre). |
+      | `size` | `num, num` | Dimensions of area. |
+      '''
+
+      self.pos = pos
+      self.size = size
+
+      self.rect = py.Rect(0, 0, 0, 0)  # NOTE remove if defaults to 0?
+
+    def update(self):
+      self.rect.width = util.slide(self.rect.width, self.size[0])
+      self.rect.height = util.slide(self.rect.height, self.size[1])
+      self.rect.centre = self.pos
+
+
+  def __init__(self, dur, text = None, highlights: list[Highlight] = None):
     '''Create a level hint.
+
+    | parameter | type | description |
+    | :-------- | :--- | :---------- |
+    | `dur` | `int` | Duration (in ticks) to display hint. |
+    | `text` | `splash.Text` | Text element to render on darkened overlay. |
+    | `highlights` | `list[Hint.Highlight]` | Part of screen to highlight. |
     '''
 
-    super().__init__(*args, **kwargs)
+    self.dur = dur
+    self.text = text
+    self.highlights = highlights
+
+    class anim:
+      tick = 0
+      alpha = util.Alpha(0, bounds = (0, 128))
+
+    self.anim = anim
+
+  def activate(self):
+    sprites.actions.add(self)
+    sprites.active.add(self, layer = sprites.layer["hints"])
+
+  def update(self):
+    if self.anim.tick:
+      self.anim.tick += 1
+      if self.anim.tick > self.dur:  # fade out
+        self.anim.alpha.alt(-4)
+        if self.anim.alpha.bounds():
+          self.kill()
+    else:  # fade in
+      self.anim.alpha.alt(4)
+        if self.anim.alpha.bounds():
+          self.anim.tick = 1
+
+    self.surf = py.Surface(screen.size, py.SRCALPHA)
+    self.surf.fill(0xffffff)
+    self.rect = self.surf.get_rect()
+
+    if self.highlights and self.anim.tick > 30:
+      for each in highlight:
+        each.update()
+        self.surf.blit(
+          source = py.Surface(each.anim.size, py.SRCALPHA),
+          dest = each.rect
+        )
+
+    if self.text:
+      self.surf.blit(self.text, self.text.rect)
+
+    self.surf.set_alpha(self.anim.alpha.value)
+
+  @ property
+  def image(self):
+    return self.surf
