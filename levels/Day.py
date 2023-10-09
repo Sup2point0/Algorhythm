3fc27683913e8082a28f0003847be1bfde623fdf
Sup#2.0 <sup2point0@gmail.com>
2023-10-09 21:19:00 +0100

    prepare new tutorial level

---
+++ levels/Day.py
@@ -0,0 +1,17 @@
+'''
+Sup#2.0 – Day
+'''
+
+from level.level import Chart
+from level.notes import TapNote, HoldNote
+from level.action import Action, Hint
+
+
+standard = Chart(
+  difficulty = 0,
+  lanes = 4,
+  keys = ["Z"],
+  data = [
+    ...
+  ]
+)
