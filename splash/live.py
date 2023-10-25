37525d4d446a189810e654002de6046efb7e5922
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-10-25 10:04:59 +0100

    add splash/live.py

---
+++ splash/live.py
@@ -0,0 +1,38 @@
+@ splash/live.py
+'''
+Implements the `run` function for handling dynamic splash functionality.
+'''
+
+from core import game, screen, sprites, config
+import util
+
+from splash.select import TrackSelect
+
+
+@ util.log(log = {None, *game.select.values()})
+def run():
+  '''Handle dynamic splash functionality for more interactive screens.'''
+
+  if screen.state.startswith("select."):
+    process.trackselect()
+
+
+class process:
+  def trackselect():
+    '''Sort track selector elements, only if necessary.'''
+
+    data = {screen.state, *game.select.values()}
+    if run.log == data:
+      return
+
+    run.log = data
+    sprites.splash["select.tracks"] = [
+      each.id for each in sorted(
+        [
+          sprite for sprite in sprites.splash[screen.state]
+          if isinstance(sprite, TrackSelect)
+        ],
+        key = config.sort[game.select["sort"]],
+        reverse = game.select["reverse"],
+      )
+    ]
