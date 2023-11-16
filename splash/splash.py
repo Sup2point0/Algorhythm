da8bbb9f5d6bcc6396a2bbed319701511602a2ac
Sup#2.0 <sup2point0@gmail.com>
2023-11-16 19:23:12 +0000

    split splash.py

--- splash/splash.py
+++ splash/splash.py
@@ -2,13 +2,9 @@
 Integrates all the different splash sprite classes into the graphical user interface.
 '''
 
-import math
-import random
-import json
-
 import pygame as py
 
-from core import game, level, screen, sprites, ui, config
+from core import game, level, screen, sprites, ui
 import util
 
 from splash import roots
@@ -22,8 +18,15 @@ from splash.select import Select, SeriesSelect, TrackSelect
 from levels import levels
 
 
+@ util.log(loaded = False)
 def setup():
   '''Load sprites for all screens in the game.'''
+
+  # safety net to avoid multiple calls
+  if setup.loaded:
+    return
+  
+  setup.loaded = True
 
   class displays:
     start = Displayed(
@@ -241,6 +244,7 @@ def setup():
       ),
       display = Displayed(
         show = {"play"},
+        align = (1, -1)
       ),
     ),
     ActiveText("level.chain",
@@ -258,126 +262,3 @@ def setup():
   score = [
     ...
   ]
-
-
-def load():
-  '''Run the game loading sequence.'''
-
-  from assets.__toppings__ import flavours
-
-  class load:
-    state = None
-    tick = 0
-    percent = random.randint(7, 20) / 100
-    alpha = util.Alpha(0)
-
-    flavour = flavours.select()
-
-  py.mixer.music.load(f"assets/tracks/dawn{random.randint(1, 3)}.mp3")
-  py.mixer.music.set_volume(0.69)
-  py.mixer.music.play()
-
-  with open("access/data.json", "r+") as file:
-    data = json.load(file)
-
-    data["game"]["runs"] += 1
-    data["game"]["version"] = game.version
-
-    util.overwrite(file, data)
-
-  ## 2.0 Studios
-  while load.tick < 240:
-    screen.display.fill((0, 0, 0))
-    
-    for event in py.event.get():
-      if event.type == py.QUIT:
-        game.state = None
-        return
-      elif event.type in [py.KEYDOWN, py.MOUSEBUTTONDOWN]:
-        if 32 < load.tick < 159:
-          load.tick = 159
-    
-    # tick variables
-    load.tick += 1
-    if 32 < load.tick < 96:
-      load.alpha.alt(4)
-    elif load.tick > 160:
-      load.alpha.alt(-4)
-
-    # render
-    rendered = Text.render("2.0 Studios", Text.Style(size = 169, col = py.Color(255, 0, 144, load.alpha.value)))
-    screen.display.blit(rendered[0], util.root(rendered[1]))
-
-    game.pulse.tick(config.rate.frames)
-    py.display.flip()
-
-  ## Loading...
-  load.state = True
-  load.tick = 0
-  while load.state is not None:
-    screen.display.fill((0, 0, 2))
-
-    for event in py.event.get():
-      if event.type == py.QUIT:
-        game.state = None
-        return
-      elif event.type in [py.KEYDOWN, py.MOUSEBUTTONDOWN]:
-        if load.percent < 1:
-          load.percent += random.randint(2, 11) / 600
-
-    # tick variables
-    if load.state is True:
-      load.tick += 1
-      if random.random() < load.percent:
-        load.percent += random.randint(2, 13) / 600
-      load.alpha.alt(4)
-    else:
-      load.state -= 1
-      if load.tick - load.state > 96:
-        load.state = None
-      load.alpha.alt(-4)
-      
-    if load.percent > 1:
-      load.percent = 1
-      load.state = load.tick
-
-    # render
-    rendered = Text.render("LOADING...", style = Text.Style(
-      col = 3 * [load.alpha.value * abs(math.cos(load.tick / 42 - 60))],
-    ))
-    screen.display.blit(rendered[0], util.root(rendered[1], y = screen.cy - 50))
-
-    rendered = Text.render(load.flavour, style = Text.Style(
-      size = 20,
-      col = 3 * [load.alpha.value],
-    ))
-    screen.display.blit(rendered[0], util.root(rendered[1], y = screen.y - 100))
-    
-    py.draw.rect(
-      surface = screen.display,
-      color = py.Color(3 * [load.alpha.value]),
-      rect = py.Rect(
-        screen.cx - screen.x / 1.6 / 2,
-        25 + screen.cy - 25 / 2,
-        load.percent * screen.x / 1.6,
-        25
-      ),
-    )
-
-    py.draw.rect(
-      surface = screen.display,
-      color = py.Color(3 * [load.alpha.value]),
-      rect = py.Rect(
-        screen.cx - (25 + screen.x / 1.6) / 2,
-        25 + screen.cy - 50 / 2,
-        25 + screen.x / 1.6,
-        50
-      ),
-      width = 5,
-    )
-
-    game.pulse.tick(config.rate.frames)
-    py.display.flip()
-  
-  screen.switch = "start"
-  screen.fade = "dark"
