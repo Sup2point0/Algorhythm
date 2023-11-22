27789b978ab1daf18934505802b83b08d9e4f9a0
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-11-22 12:20:57 +0000

    add `tap.py` to `level/notes/`

---
+++ level/notes/tap.py
@@ -0,0 +1,71 @@
+'''
+Implements the `TapNote` note kind.
+'''
+
+import pygame as py
+
+from core import config
+
+from effects import blur
+from effects.pop import PopEffect
+
+
+class TapNote(Note):
+  '''A note hit by a pressed key.'''
+
+  def __init__(self, hit, **kwargs):
+    '''Create a tap note.
+    
+    | parameter | type | description |
+    | :-------- | :--- | :---------- |
+    | `hit` | `num` | Beat note should be hit on. |
+
+    Other base parameters are inherited from `Note`.
+    '''
+
+    super().__init__(**kwargs)
+
+    self.hit = hit
+
+    self.size = [
+      config.note.size[0] * opt.note.size(),
+      config.note.size[1]
+    ]
+
+  def spawn(self):
+    '''Spawn a tap note.'''
+
+    super().spawn()
+
+    ## render
+    self.surf = blur.glow(
+      size = self.size,
+      dist = 50,
+      col = self.col,
+      blur = 40,
+    )
+
+    py.draw.rect(
+      surface = self.surf,
+      color = py.Color(0xffffffff),
+      rect = py.Rect(50, 50, *self.size),
+      width = 0,
+      border_radius = round(min(self.size) // 2),
+    )
+    
+    self.rect = self.surf.get_rect()
+
+  def pop(self, hit = False) -> str | None:
+    '''Delete the note and return accuracy.
+    
+    `hit` determines if it was hit by the player.
+    '''
+    
+    acc = self.accuracy(level.beat, self.hit) if hit else "miss"
+
+    if acc:
+      super().pop(acc)
+      if acc != "miss":
+        PopEffect(pos = self.pos, acc = acc)
+
+    return acc
