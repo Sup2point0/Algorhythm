61579afba0f6f63a84beeb8a718327fc778670c7
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-11-22 12:27:52 +0000

    add `hold.py` to `level/notes/`

---
+++ level/notes/hold.py
@@ -0,0 +1,55 @@
+'''
+Implements the `HoldNote` class.
+'''
+
+import pygame as py
+
+from level.notes import Note
+
+
+class HoldNote(Note):
+  '''A note hit by a pressed and held key.'''
+
+  def __init__(self, hit, **kwargs):
+    '''Create a hold note.
+    
+    | parameter | type | description |
+    | :-------- | :--- | :---------- |
+    | `hit` | `num, num` | Beat note should be hit on, and beat it should be held until. |
+
+    Other base parameters are inherited from `Note`.
+    '''
+
+    super().__init__(**kwargs)
+
+    self.hit = hit
+    if hit[0] >= hit[1]:
+      raise ValueError("hold note cannot end before it starts")
+
+    self.popped = False
+    self.slipped = False
+    self.popping = False
+    self.poptick = 0
+
+  def update(self):
+    if not self.popping:
+      if level.beat > self.hit[0]:  # note missed
+        if self.accuracy(level.beat, self.hit[0]) == "miss":
+          self.slipped = True
+    
+    else:
+      keys = py.key.get_pressed()
+      key = config.keys[self.lane.key]
+      if not keys[key]:
+        self.poptick += 1
+        if self.poptick > 2:  # key slipped
+          self.slipped = True
+      else:
+        acc = self.accuracy(level.beat, self.hit[1])
+        if acc and acc != "miss":  # popped within hit timing
+          self.popped = True
+
+  def pop(self, hit = False):
+    '''Start popping note.'''
+
+    self.popping = True
