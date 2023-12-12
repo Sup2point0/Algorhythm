6de3c919769fda310f0498d7c1870438173acabf
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-12-12 15:46:04 +0000

    add `splash/presets.py`

---
+++ source/splash/presets.py
@@ -0,0 +1,30 @@
+'''
+Implements style and display presets.
+'''
+
+from splash import roots
+from splash.elements import Displayed
+from splash.text import Text
+
+
+class style:
+  '''Style setting presets.'''
+
+  title = Text.Style(
+    typeface = ui.font.title,
+    size = 40,
+  )
+
+
+class display:
+  '''Display setting presets.'''
+
+  start = Displayed(
+    show = {"start"},
+    align = (-1, 0),
+  )
+
+  credits = Displayed(
+    show = {"settings.credits"},
+    scroll = {roots.scroll("settings.credits")},
+  )
