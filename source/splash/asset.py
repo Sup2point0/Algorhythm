5df5c48e82c25db698201609d24ec7100b9f3231
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-12-19 15:25:27 +0000

    fixes and updates to `asset.py`

--- source/splash/asset.py
+++ source/splash/asset.py
@@ -1,10 +1,11 @@
 '''
-Implements the `Asset` class, for image sprites.
+Implements the `Asset` class for image sprites.
 '''
 
 import pygame as pg
 
 from innate import Alpha
+import util
 
 from splash.elements import Element
 
@@ -12,10 +13,10 @@ import effects.blur
 
 
 class Asset(Element):
-  '''An image–only splash element.'''
+  '''An image sprite.'''
 
   def __init__(self, id, pos,
-    image: str,
+    image,
     size = None,
     blur = None,
     dark = None,
@@ -23,19 +24,19 @@ class Asset(Element):
   ):
     '''Create an image element.
 
-    ｜ parameter | type | description |
-    | :--------- | :-- | :----------- |
-    | `image` | The file name from which to load the image asset. |
-    | `size` | `int, int` | Resize image if specified. |
-    | `blur` | Amount of blur to apply to image. |
-    | `dark` | Alpha value of dark cover to overlay on image. |
+    ｜ parameter | type | notes |
+    | :--------- | :--- | :---- |
+    | `image` | `str` | The file (path) from which to load the image asset. |
+    | `size`  | `int, int` | Resize image if specified. |
+    | `blur`  | Amount of blur to apply to image. |
+    | `dark`  | Alpha value of dark cover to overlay on image. |
 
     Other base parameters are inherited from `splash.Element`.
     '''
 
     super().__init__(id, pos, display = display)
 
-    self.surf = pg.image.load(f"assets/{image}")
+    self.surf = util.find.asset(image)
     if size:
       self.surf = pg.transform.scale(self.surf, size)
     if blur:
@@ -44,7 +45,7 @@ class Asset(Element):
 
     if dark is not None:
       self.cover = pg.Surface(self.surf.get_size())
-      self.cover.fill(0x000000)
+      self.cover.fill(((0, 0, 0)
       self.cover.set_alpha(Alpha(dark)())
       self.surf.blit(self.cover, (0, 0))
 
