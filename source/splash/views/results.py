3c912228c39b7155457e44a1ba196979f7c16eb2
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-12-21 18:00:33 +0000

    continue implementing `LevelResults`

--- source/splash/views/results.py
+++ source/splash/views/results.py
@@ -15,16 +15,25 @@ class LevelResults(Element):
   '''A collection of splash elements for displaying level results.'''
 
   def __init__(self, id, pos, display = None):
-    '''Create a level results view.'''
+    '''Create a level results view.
+    
+    All parameters are base parameters inherited from `splash.Element`.
+    '''
 
     super().__init__(id, pos, anim = True, display = display)
 
     self.size = ui.size.views.results
     self.style = Text.Style(size = 25,
-      col = ui.col.text.idle[:3] + (0,),
+      col = ui.col.text.idle[:3] + [0],
     )
 
     self.rect = pg.Rect(pos, self.size)
+
+    class layout:
+      left = 50
+      space = 25
+
+    self.layout = layout
 
     self.reset()
     super().position()
@@ -35,8 +44,11 @@ class LevelResults(Element):
     self.anim.state = True
     self.anim.tick = 0
 
-    self.anim.x = [0, 0, 0]
-    '''Width of 3 backing rectangles.'''
+    rects = []
+    rects.append([0, 0, 0, 100])
+    rects.append([0, rects[0].h + 50, 0, 200])
+    rects.append([0, rects[1].y + rects[1].h + 50, 0, 600])
+    self.anim.rects = rects
     
     self.anim.styles = {
       "score": Text.Style(typeface = "title", size = 50),
@@ -50,36 +62,43 @@ class LevelResults(Element):
 
   def update(self):
     self.anim.tick += 1
+    styles = self.anim.styles
     
     self.surf = pg.Surface(self.size, pg.SRCALPHA)
 
     ## backing rectangles
     if self.anim.tick > 15:
       self._slide_(0)
-      pg.draw.rect(self.surf, ui.col.back, rect = (0, 0, self.anim.x[0], 100))
+      pg.draw.rect(self.surf, ui.col.back, rect = self.anim.rects[0])
     
     if self.anim.tick > 30:
       self._slide_(1)
-      pg.draw.rect(self.surf, ui.col.back, rect = (0, 150, self.anim.x[1], 200))
+      pg.draw.rect(self.surf, ui.col.back, rect = self.anim.rects[1])
     
     if self.anim.tick > 45:
       self._slide_(2)
-      pg.draw.rect(self.surf, ui.col.back, rect = (0, 400, self.anim.x[2], 600))
-
+      pg.draw.rect(self.surf, ui.col.back, rect = self.anim.rects[2])
+      
     ## score
-    if self.anim.tick > 120:
-      ...
-      self.surf.blit(Text.render(level.score, self.anim.styles["score"])[0], dest = (20, 20))
+    if self.anim.tick > 30:
+      self._fade_(styles["score"])
+      self._render_((20, 20), styles["score"], level.scored)
 
-    ## ...
-    if self.anim.tick > 0:
-      self._render_("CHAIN", (0, 200))
-    if self.anim.tick > 0:
-      self._render_("PRECISION", (0, 200))
-    if self.anim.tick > 0:
-      self._render_("ACCURACY", (0, 200))
+    ## info
+    if self.anim.tick > 45:
+      style = styles["info"][0]
+      self._fade_(style)
+      self._render_((self.layout.left, 175), style, "CHAIN")
+      self._render_((self.layout.left, 200), style, "PRECISION")
+      self._render_((self.layout.left, 225), style, "ACCURACY")
+    if self.anim.tick > 60:
+      style = styles["info"][1]
+      self._fade_(style)
+      self._render_((self.layout.left, 175), style, "CHAIN")
+      self._render_((self.layout.left, 200), style, "PRECISION")
+      self._render_((self.layout.left, 225), style, "ACCURACY")
 
-    ## breakdown
+    ## details
     if self.anim.tick > 0:
       self._render_("HIT", (0, 200))
     if self.anim.tick > 0:
@@ -99,13 +118,17 @@ class LevelResults(Element):
         self._render_("FIXED", (0, 200))
 
   def _slide_(self, idx):
-    '''Utility function for animating backing rectangles.'''
+    '''Animate backing rectangle.'''
 
-    self.anim.x[idx] = util.slide(self.anim.x[idx], self.size[0], speed = 4)
+    rect = self.anim.rects[idx]
+    rect.w = util.slide(rect.w, self.size[0], speed = 4)
 
-  def _render_(self, tick, text, pos, style):
-    '''Utility function to render text to the sprite.'''
+  def _fade_(self, style):
+    '''Increase alpha of text style for fade-in animation.'''
 
-    if self.anim.tick > tick:
-      style.col[3] += config.rate.fade
-      self.surf.blit(Text.render(text, style)[0], dest = pos)
+    style.col[3] += config.fade.rate
+
+  def _render_(self, pos, style, text):
+    '''Render text to the sprite (has a cleaner argument order).'''
+
+    self.surf.blit(Text.render(text, style)[0], dest = pos)
